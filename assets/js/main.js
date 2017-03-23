8760d5c0f7572dd7e5841b372cdd962e784d381e
LeidyRosmery <rosm3rita@hotmail.com>
2017-03-23 00:16:32 -0500

    funciona todos los botones

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,73 +1,78 @@
-var textPost = document.getElementById("texto");
-var btnPublicar = document.getElementById("botonPublicar");
-var estado = document.getElementById("seleccionEstado");
-var div = document.getElementById("divMostrar");
+  var btnPublicar = document.getElementById("botonPublicar");
   var miPost1 = new miPost();
-function miPost() {
-    this.array = [];
-    this.contador = 0;
-    this.crearPost = function(texto1, estado1) {
-        this.array.push({
-            id: this.contador,
-            texto: texto1,
-            estado: estado1
-        })
-        this.contador++;
-    }
-    //*************************
-    this.publicaPost = function() {
-  console.log(this.contador);
-        this.array.forEach(function(elemento) {
-            var fieldset = document.createElement("fieldset");
-            fieldset.style = "width : 250px";
-            var textarea = document.createElement("textarea");
-            textarea.cols = "60";
-            textarea.innerHTML = elemento.texto;
-            textarea.setAttribute("disabled", true);
-            var editar = document.createElement("a");
-            editar.innerHTML = "Editar";
-            editar.setAttribute("href", "#");
-            var cambiarBoton = 0;
-            editar.addEventListener('click', function(e) {
-                console.log(e.currentTarget);
-                if (cambiarBoton == 0) {
-                    editar.innerHTML = "Guardar";
-                    textarea.disabled = false;
-                    cambiarBoton = 1;
+
+  function miPost() {
+      this.array = [];
+      this.contador = 0;
+      this.crearPost = function(texto, estado) {
+          this.array.push({
+              id: this.contador,
+              texto: texto,
+              estado: estado
+          })
+          this.contador++;
+      }
+
+      //****************
+      this.creaPost = function(parent) {
+          parent.innerHTML = "";
+          this.array.forEach(function(elemento) {
+              parent.appendChild(this.publicaPost(elemento.texto, elemento.id));
+          }, this);
+      }
+      //*************************
+      this.publicaPost = function(text, id) {
+          var fieldset = document.createElement("fieldset");
+          fieldset.style = "width : 250px";
+          fieldset.setAttribute("data-id", id);
+          var textarea = document.createElement("textarea");
+          textarea.cols = "60";
+          textarea.innerHTML = text;
+          textarea.setAttribute("disabled", true);
+          var editar = document.createElement("a");
+          editar.innerHTML = "Editar";
+          editar.setAttribute("href", "#");
+          var cambiarBoton = 0;
+          editar.addEventListener('click', function(e) {
+              if (cambiarBoton == 0) {
+                  editar.innerHTML = "Guardar";
+                  textarea.disabled = false;
+                  cambiarBoton = 1;
                 } else {
-                    editar.innerHTML = "Editar";
-                    textarea.disabled = true;
-                    cambiarBoton = 0;
-                }
-            });
-            var eliminar = document.createElement("a");
-            eliminar.innerHTML = "Eliminar";
-            eliminar.setAttribute("href", "#");
 
-            eliminar.addEventListener('click', function() {
-                alert("Esta seguro de eliminar la publicacion");
+                  editar.innerHTML = "Editar";
+                  textarea.disabled = true;
+                  cambiarBoton = 0;
+                  var postId = e.target.parentNode.getAttribute('data-id');
+                miPost1.array[postId].texto=textarea.value
+              }
+
+
+          });
+          var eliminar = document.createElement("a");
+          eliminar.innerHTML = "Eliminar";
+          eliminar.setAttribute("href", "#");
+          eliminar.addEventListener('click', function(e) {
+              alert("Esta seguro de eliminar la publicacion");
               if (fieldset.parentNode) {
-                    fieldset.parentNode.removeChild(fieldset);
-                }
-                console.log(miPost1.contador);
-                console.log(  miPost1.array.splice(miPost1.contador-1,1));
-                miPost1.array.splice(miPost1.array.id,1);
-            });
-            fieldset.appendChild(textarea);
-            fieldset.appendChild(editar);
-            fieldset.appendChild(eliminar);
-            div.appendChild(fieldset);
-        });
-    }
-}
+                  fieldset.parentNode.removeChild(fieldset);
+              }
+              var postId = e.target.parentNode.getAttribute('data-id');
+              miPost1.array.splice(postId, 1);
+          });
+          fieldset.appendChild(textarea);
+          fieldset.appendChild(editar);
+          fieldset.appendChild(eliminar);
+          return fieldset;
+      }
+  }
 
-function limpiar() {
-    textPost.value = "";
-}
-btnPublicar.addEventListener('click', function() {
-    divMostrar.innerHTML="";
-    miPost1.crearPost(textPost.value, estado.value);
-    miPost1.publicaPost();
-    console.log(miPost1.array);
-    limpiar();
-})
+
+  btnPublicar.addEventListener('click', function() {
+      var textPost = document.getElementById("texto");
+      var estado = document.getElementById("seleccionEstado");
+      var div = document.getElementById("divMostrar");
+      miPost1.crearPost(textPost.value, estado.value);
+      miPost1.creaPost(div);
+      textPost.value = "";
+  });
