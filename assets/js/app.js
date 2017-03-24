ef07b1b2f972710ecacddbbd336248083e789193
LeidyRosmery <rosm3rita@hotmail.com>
2017-03-23 20:00:53 -0500

    corregit add .

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,29 @@
-var textPost = document.getElementById("texto");
+var micontador=-1;
   function handleFileSelect(evt) {
+console.log(micontador);
     var files = evt.target.files; // FileList object
-
     // Loop through the FileList and render image files as thumbnails.
     for (var i = 0, f; f = files[i]; i++) {
-
       // Only process image files.
       if (!f.type.match('image.*')) {
+        alert("esto no es una imagen");
         continue;
       }
-
       var reader = new FileReader();
-
       // Closure to capture the file information.
       reader.onload = (function(theFile) {
         return function(e) {
           // Render thumbnail.
-          //var span = document.createElement('span');
-          textPost.value = ['<img class="thumb" src="', e.target.result,
+          var span = document.createElement('span');
+          span.setAttribute("id",micontador);
+          span.innerHTML = ['<img class="thumb" src="', e.target.result,
                             '" title="', escape(theFile.name), '"/>'].join('');
-          //document.getElementById('list').insertBefore(span, null);
+          document.getElementById('imagenDiv').insertBefore(span, null);
         };
       })(f);
-
       // Read in the image file as a data URL.
       reader.readAsDataURL(f);
     }
+  micontador++;
   }
-
   document.getElementById('files').addEventListener('change', handleFileSelect, false);
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,15 @@
   var btnPublicar = document.getElementById("botonPublicar");
   var miPost1 = new miPost();
-
+ var contador=0;
   function miPost() {
       this.array = [];
       this.contador = 0;
-      this.crearPost = function(texto, estado) {
+      this.crearPost = function(texto, estado,imagen) {
           this.array.push({
               id: this.contador,
               texto: texto,
-              estado: estado
+              estado: estado,
+              imagen:imagen
           })
           this.contador++;
       }
@@ -17,11 +18,13 @@
       this.creaPost = function(parent) {
           parent.innerHTML = "";
           this.array.forEach(function(elemento) {
-              parent.appendChild(this.publicaPost(elemento.texto, elemento.id));
+              parent.appendChild(this.publicaPost(elemento.texto, elemento.id,elemento.imagen));
           }, this);
       }
       //*************************
-      this.publicaPost = function(text, id) {
+
+      //**********************************************************
+      this.publicaPost = function(text, id,img) {
           var fieldset = document.createElement("fieldset");
           fieldset.style = "width : 250px";
           fieldset.setAttribute("data-id", id);
@@ -29,6 +32,9 @@
           textarea.cols = "60";
           textarea.innerHTML = text;
           textarea.setAttribute("disabled", true);
+          var miPic= document.createElement("img");
+          miPic.setAttribute("src",img);
+        miPic.setAttribute("class","thumb");
           var editar = document.createElement("a");
           editar.innerHTML = "Editar";
           editar.setAttribute("href", "#");
@@ -58,16 +64,23 @@
               miPost1.array.splice(postId, 1);
           });
           fieldset.appendChild(textarea);
+          fieldset.appendChild(miPic);
           fieldset.appendChild(editar);
           fieldset.appendChild(eliminar);
           return fieldset;
       }
   }
+
   btnPublicar.addEventListener('click', function() {
+
       var textPost = document.getElementById("texto");
       var estado = document.getElementById("seleccionEstado");
       var div = document.getElementById("divMostrar");
-      miPost1.crearPost(textPost.value, estado.value);
+      var miSpan=document.getElementById(contador);
+      var nombreImagen=miSpan.firstChild.src;
+      miPost1.crearPost(textPost.value, estado.value,nombreImagen);
       miPost1.creaPost(div);
       textPost.value = "";
+     miSpan.innerHTML="";
+     contador++;
   });
